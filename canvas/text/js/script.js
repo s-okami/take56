9bc0d48f8df51451f84e2643a778e0df45978239
s.okami <s.okami@PC-150.local>
2013-05-21 12:45:02 +0900

    how to use google web font in canvas

---
+++ canvas/text/js/script.js
@@ -0,0 +1,21 @@
+$(window).load(function() {
+    
+    var canvas = $('#screen')[0];
+    if (!canvas || !canvas.getContext) return false;
+    var context = canvas.getContext('2d');
+    var image = new Image();
+    image.src = "/img/sample.jpg";
+    
+    image.onload = function(e) {
+        context.drawImage(image, 0, 0);
+        context.translate(canvas.width * 0.5, canvas.height * 0.5);
+        context.rotate(16 * Math.PI / 180);
+        context.fillStyle = '#f2f2f2';
+        context.font = "100pt 'Chewy', cursive";
+        context.textAlign = 'center';
+        context.textBaseline = 'middle';
+        context.fillText('SAMPLE', 0, 0);
+        context.rotate(-16 * Math.PI / 180);
+        context.translate(-canvas.width * 0.5, -canvas.height * 0.5);
+    }
+});
